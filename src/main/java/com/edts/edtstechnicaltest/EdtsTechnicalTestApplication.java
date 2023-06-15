4bfd2c33f67ae13245632ac1831c5a8a2cc547b2
viaann <darkness14042@gmail.com>
2023-06-15 19:00:58 +0700

    init project

---
+++ src/main/java/com/edts/edtstechnicaltest/EdtsTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.edts.edtstechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtsTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtsTechnicalTestApplication.class, args);
+	}
+
+}
